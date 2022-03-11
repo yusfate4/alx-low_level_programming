db6e5bf9d2ff4a2437bded7f30392cec8ebc0b69
yusfate4 <abiodunyusuf4@gmail.com>
2022-03-11 13:20:26 -0800

    else if statement

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,22 @@
-include <stdio.h>
-
+#include <stdio.h>
 /**
-* main - Print all possible combinations of single digit numbers
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int number;
+int numb;
 
-		for (number = 0; number <= 9; number++)
-		{
-		putchar(number + '0');
-		if (number < 9)
-		{
-		putchar(',');
-		putchar(' ');
-		}
-		}
-		putchar('\n');
-
-	return (0);
+for (numb = '0' ; numb <= '9' ; numb++)
+{
+putchar(numb);
+if (numb != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
