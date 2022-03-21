5b7cbc5a36c13c297e3efcc947f50be21062c51c
yusfate4 <abiodunyusuf4@gmail.com>
2022-03-21 09:28:42 -0700

    added

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 void reset_to_98(int *n);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -8,3 +11,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif
