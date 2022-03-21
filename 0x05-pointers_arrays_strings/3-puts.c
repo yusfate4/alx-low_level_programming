e023c114d3c75d017f847bb90c4d54f61d0a8d21
yusfate4 <abiodunyusuf4@gmail.com>
2022-03-21 09:24:45 -0700

    added

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _puts - prints a string
- * @str: the string
+ * _puts - main function.
+ * @str: Pointer with the address value.
+ * Description: This function takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * Return: none.
  */
-
 void _puts(char *str)
 {
-int i = 0;
-
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
