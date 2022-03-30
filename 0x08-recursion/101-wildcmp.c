1ff272d4352efa3e42571f2c76d5172b08727182
Dahud Yusuf Ishola <65573250+yusfate4@users.noreply.github.com>
2022-03-31 00:41:49 +0100

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,48 @@
-#include "main.h"
-
+int recursive_check(char *s1, char *s2, int wildC);
 /**
- * wildcmp - Funtion
+ * wildcmp - compares two strings, supports wildcard '*'
  *
- * @s1: Variable
- * @s2: Variable
- *
- * Return: Int
+ * @s1: string 1 to compare to s2
+ * @s2: string 2 to compare to s1
+ * @wildC: counter to see if wildcard is present
+ * Return: 1 if identical, 0 otherwise, -1 for error
  */
 int wildcmp(char *s1, char *s2)
 {
-
-	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
-		return (wildcmp(s1 + 1, s2 + 1));
+	return (recursive_check(s1, s2, 0));
+}
+/**
+ * recursive_check - checks for wild cards recursively
+ *
+ * @s1: string 1 to check (no wild cards)
+ * @s2: string to to check (can contain wildcards)
+ * @wildC: counter to see if wildcard is present
+ *
+ * Return: 1 if identical, 0 for not, -1 for error
+ */
+int recursive_check(char *s1, char *s2, int wildC)
+{
 	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
-		if (*(s1 + 1) != '\0' && *s2 == '\0')
-			return (0);
+		return (1);
 	}
-	return (0);
+	if (*s2 == '*')
+		return (recursive_check(s1, s2 + 1, ++wildC));
+	else if (*s1 != *s2 && wildC > 0)
+	{
+		if (!*s1)
+			return (0);
+		return (recursive_check(s1 + 1, s2, wildC));
+	}
+	else if (*s1 == *s2 && wildC == 0)
+		return (recursive_check(s1 + 1, s2 + 1, 0));
+	else if (*s1 == *s2 && wildC > 0)
+	{
+		if (!recursive_check(s1 + 1, s2 + 1, 0))
+			return (recursive_check(s1 + 1, s2, wildC));
+		return (recursive_check(s1 + 1, s2 + 1, wildC));
+	}
+	else if (*s1 != *s2 && wildC == 0)
+		return (0);
+	return (-1);
 }
