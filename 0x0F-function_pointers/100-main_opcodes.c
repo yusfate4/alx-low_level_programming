3c5958c6182cf8ae0103f06be641478c74426e73
yusfate4 <abiodunyusuf4@gmail.com>
2022-04-13 09:22:42 -0700

    C - Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,39 @@
-#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * op_sub -
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: 
+ * Return: Always 0.
  */
-int op_sub(int a, int b)
+int main(int argc, char *argv[])
 {
+	char *opc = (char *) main;
+	int i, nbytes;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
 }
